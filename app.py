06f9d8dd24cdcb1050801d3c0f27f769cbb518ff
NishantGarg27111999 <nishantgarg17282@gmail.com>
2022-11-24 23:01:28 +0530

    8

--- app.py
+++ app.py
@@ -85,5 +85,7 @@ def upload():
         return result
     return None
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug = True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
